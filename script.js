634678433935a3ab73d72f450fecf2f76bb7ebb0
aldomanduca <aldo.manduca@gmail.com>
2021-10-01 20:42:05 +0200

    Start/finish selectors
    Added path start and finish selector

--- script.js
+++ script.js
@@ -11,17 +11,47 @@ function makeGrid() {
 
         for (let j = 0; j < 11; j++) {
             let myCell = document.createElement("td");
-            myCell.setAttribute(`data-index`,`${i},${j}`)
+            myCell.setAttribute(`data-index`, `${i},${j}`)
+            console.log(`data-index`, `${i},${j}`);
             rowW.appendChild(myCell);
         }
-
     }
-
 }
+
+let startSelected = false;
+let endSelected = false;
 
 function tes(event) {
     if (event.target.nodeName == "TD") {
-      console.log(event.target.getAttribute('data-index'));
-    }
-  }
+        const dataIndex = event.target.getAttribute('data-index');
+        console.log(dataIndex);
+        const indexesArray = dataIndex.split(",");
+        //console.log(parseInt(event.target.getAttribute('data-index')))
+        const indexes = [parseInt(indexesArray[0]), parseInt(indexesArray[1])];
+        if (!startSelected) {
+            SelectStart(indexes);
+        } else if (!endSelected) {
+            SelectEnd(indexes);
+        }
 
+        
+    return indexes;
+    }
+}
+  
+function SelectStart(indexes) {
+    let startNode = document.querySelectorAll(`[data-index="${indexes[0]},${indexes[1]}"]`)[0];
+    startNode.style.backgroundColor = "blue";
+    startSelected = true;
+    startNode.setAttribute('isStart', true);
+}
+
+function SelectEnd(indexes) {
+    let endNode = document.querySelectorAll(`[data-index="${indexes[0]},${indexes[1]}"]`)[0];
+    console.log(endNode);
+    if (!endNode.getAttribute('isStart')) {
+        endNode.style.backgroundColor = "red";
+        endSelected = true;
+        endNode.setAttribute('isEnd', true);
+    }
+}
